7310256441c83d1a1c96931e6c217e3b51cbcf17
Garren Gaut <ggaut@lrwonline.com>
2021-11-07 11:30:46 -0800

    adding real test

---
+++ R/test_function.R
@@ -0,0 +1,7 @@
+#' Test Function
+#' @description prints a string
+#' @param s character what to print
+#' @export
+test_function <- function(s = "GREAT") {
+  s
+  }
---
+++ tests/testthat/test-test_function.R
@@ -0,0 +1,3 @@
+test_that("multiplication works", {
+  expect_equal(test_function("GARREN"), "GARREN")
+})
